c3029812d2ca176da28d98a4dc58f1b67cb9f81f
Andrew Harris <aharris86@gmail.com>
2016-04-25 17:25:23 -0400

    initial commit

---
+++ slackMessagecounter/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  slackMessagecounter
+//
+//  Created by Andrew Harris on 4/25/16.
+//  Copyright © 2016 Andrew Harris. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBOutlet weak var KeyCountField: NSTextField!
+
+    @IBAction func someButton(sender: AnyObject) {
+        let txt = "something"
+        KeyCountField.stringValue = txt
+    }
+    
+    
+}
+
