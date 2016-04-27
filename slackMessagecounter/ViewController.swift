0b16452510fe749fe333fa1fdd36c92f21191d30
Andrew Harris <aharris86@gmail.com>
2016-04-27 16:15:16 -0400

    setup for queries

--- slackMessagecounter/ViewController.swift
+++ slackMessagecounter/ViewController.swift
@@ -7,10 +7,6 @@
 //
 
 
-//TODO: make https://slack.com/api/search.messages?token=xoxp-3153534091-37521654836-37628781536-40cdbbe841&query=from:me after:2016-04-26&pretty=1 easier to adjust - specifically the date
-//TODO: Add a date picker
-//TODO: run last 7 days and last 1 day and display both
-
 import Cocoa
 
 class ViewController: NSViewController {
@@ -26,6 +22,7 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
     @IBOutlet weak var dateDisplay: NSTextField!
     @IBOutlet weak var todayCount: NSTextField!
     @IBOutlet weak var weekCount: NSTextField!
@@ -35,8 +32,8 @@ class ViewController: NSViewController {
         dateDisplay.stringValue = setTime();
     }
     
-   
     
+//TODO: consider expanding this to return just the date or just the time based on a param
     func setTime() -> String {
         let date = NSDate()
         let dateFormatter = NSDateFormatter()
@@ -45,5 +42,11 @@ class ViewController: NSViewController {
         return dateFormatter.stringFromDate(date)
     }
     
-
+    func querySlack() {
+//TODO: make https://slack.com/api/search.messages?token=xoxp-3153534091-37521654836-37628781536-40cdbbe841&query=from:me after:2016-04-26&pretty=1 easier to adjust - specifically the date
+//TODO: run last 7 days and last 1 day and display both
+        
+    }
+    
+    
 }
