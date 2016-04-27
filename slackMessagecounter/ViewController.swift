4c7eac9ce9ba174e4667e322e5813ba43122420a
Andrew Harris <aharris86@gmail.com>
2016-04-27 15:58:33 -0400

    sets layout sets datetime

--- slackMessagecounter/ViewController.swift
+++ slackMessagecounter/ViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2016 Andrew Harris. All rights reserved.
 //
 
+
+//TODO: make https://slack.com/api/search.messages?token=xoxp-3153534091-37521654836-37628781536-40cdbbe841&query=from:me after:2016-04-26&pretty=1 easier to adjust - specifically the date
+//TODO: Add a date picker
+//TODO: run last 7 days and last 1 day and display both
+
 import Cocoa
 
 class ViewController: NSViewController {
@@ -21,17 +26,24 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    @IBOutlet weak var dateDisplay: NSTextField!
+    @IBOutlet weak var todayCount: NSTextField!
+    @IBOutlet weak var weekCount: NSTextField!
 
 
     @IBAction func someButton(sender: AnyObject) {
-        let txt = "something"
-//FIX:         KeyCountField.stringValue = txt
+        dateDisplay.stringValue = setTime();
     }
     
-    //TODO: make https://slack.com/api/search.messages?token=xoxp-3153534091-37521654836-37628781536-40cdbbe841&query=from:me after:2016-04-26&pretty=1 easier to adjust - specifically the date
-    //TODO: Add a date picker
-    //TODO: run last 7 days and last 1 day and display both
+   
     
+    func setTime() -> String {
+        let date = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss"
+        
+        return dateFormatter.stringFromDate(date)
+    }
     
+
 }
