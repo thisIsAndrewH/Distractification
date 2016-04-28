af856d46e745b6a44fdfc3612a7da03aa22ca96c
Andrew Harris <aharris86@gmail.com>
2016-04-28 16:05:09 -0400

    makes qpi request

--- slackMessagecounter/ViewController.swift
+++ slackMessagecounter/ViewController.swift
@@ -29,7 +29,9 @@ class ViewController: NSViewController {
 
 
     @IBAction func someButton(sender: AnyObject) {
-        dateDisplay.stringValue = setTime();
+        dateDisplay.stringValue = setTime()
+        
+        querySlack("2016-04-26")
     }
     
     
@@ -42,10 +44,40 @@ class ViewController: NSViewController {
         return dateFormatter.stringFromDate(date)
     }
     
-    func querySlack() {
+    func querySlack(dateAfter: String) {
 //TODO: make https://slack.com/api/search.messages?token=xoxp-3153534091-37521654836-37628781536-40cdbbe841&query=from:me after:2016-04-26&pretty=1 easier to adjust - specifically the date
-//TODO: run last 7 days and last 1 day and display both
+
+        //TODO: Move somewhere safe
+        let token = "xoxp-3153534091-37521654836-37628781536-40cdbbe841"
+        let endpoint = "https://slack.com/api/search.messages?token=" + token + "&pretty=1&query=from:me%20after:" + dateAfter
         
+        let request = NSMutableURLRequest(URL: NSURL(string: endpoint)!)
+        httpGet(request){
+            (data, error) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                print(data)
+                //TODO: Manipulate this somehow
+            }
+        }
+        
+        
+    }
+    
+    func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request){
+            (data, response, error) -> Void in
+            if error != nil {
+                callback("", error!.localizedDescription)
+            } else {
+                let result = NSString(data: data!, encoding:
+                    NSASCIIStringEncoding)!
+                callback(result as String, nil)
+            }
+        }
+        task.resume()
     }
     
     
